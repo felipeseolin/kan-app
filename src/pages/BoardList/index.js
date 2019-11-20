ec147b6bb16295238a7e7bf07405db93627159a3
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-11-19 22:14:17 -0200

    Rename components

--- src/pages/BoardList/index.js
+++ src/pages/BoardList/index.js
@@ -1,27 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const BoardList = () => {
+import api from '../../services/api';
+
+function BoardList() {
+  const [boards, setBoards] = useState([]);
+
   return (
     <div className="container">
       <h1>Titulo</h1>
-
       <a href="/boards/new" className="btn btn-success">Novo quadro</a>
-
       <div className="card m-3">
-        <a href="/boards/{{ this.id }}">
+        <a href="/boards/asdfasd">
             <div className="card-body">
                 <h4>Nome</h4>
                 <p>Descricao</p>
             </div>
             <div className="float-right">
                 <div className="m-2 float-right">
-                    <a href="/boards/{{ this.id }}" className="btn btn-sm btn-primary">Visualizar</a>
+                    <a href="/boards/this.id" className="btn btn-sm btn-primary">Visualizar</a>
                 </div>
                 <div className="m-2 float-right">
-                    <a href="/boards/edit/{{ this.id }}" className="btn btn-sm btn-secondary">Editar</a>
+                    <a href="/boards/edit/this.id" className="btn btn-sm btn-secondary">Editar</a>
                 </div>
                 <div className="m-2 float-right">
-                    <a href="/boards/destroy/{{ this.id }}" className="btn btn-sm btn-danger">Excluir</a>
+                    <a href="/boards/destroy/hi" className="btn btn-sm btn-danger">Excluir</a>
                 </div>
             </div>
           </a>
