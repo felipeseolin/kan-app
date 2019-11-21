4aa5dc92da46d5e047e381ae505974040d144d5e
felipeseolin <felipe.seolin@hotmail.com>
2019-11-21 17:41:38 -0300

    Board Create and Edit Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // Components
 import Login from './pages/Login';
 import BoardList from './pages/BoardList';
+import BoardCreate from './pages/BoardCreate';
+import BoardEdit from './pages/BoardEdit';
 
 const App = () => {
   return (
@@ -18,9 +16,15 @@ const App = () => {
         <Route exact path="/boards">
           <BoardList />
         </Route>
+        <Route exact path="/boards/new">
+          <BoardCreate />
+        </Route>
+        <Route exact path="/boards/:id/edit">
+          <BoardEdit />
+        </Route>
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
