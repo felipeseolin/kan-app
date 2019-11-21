094867120dd72a9c411cedaea779b5485ac529dc
felipeseolin <felipe.seolin@hotmail.com>
2019-11-21 17:41:14 -0300

    Connection to API

--- src/pages/BoardList/index.js
+++ src/pages/BoardList/index.js
@@ -1,36 +1,45 @@
 import React, { useState, useEffect } from 'react';
 
+import BoardItem from '../../components/BoardItem';
 import api from '../../services/api';
 
-function BoardList() {
+const BoardList = () => {
   const [boards, setBoards] = useState([]);
+
+  const fetchBoards = async () => {
+    const response = await api.get('boards');
+    setBoards(response.data);
+  };
+
+  useEffect(() => {
+    fetchBoards();
+  }, []);
+
+  const handleDelete = id => {
+    api
+      .delete(`/boards/${id}`)
+      .then(() => alert('Quadro excluido'))
+      .catch(() => alert('Erro ao excluir'))
+      .finally(() => window.location.reload());
+  };
 
   return (
     <div className="container">
       <h1>Titulo</h1>
-      <a href="/boards/new" className="btn btn-success">Novo quadro</a>
-      <div className="card m-3">
-        <a href="/boards/asdfasd">
-            <div className="card-body">
-                <h4>Nome</h4>
-                <p>Descricao</p>
-            </div>
-            <div className="float-right">
-                <div className="m-2 float-right">
-                    <a href="/boards/this.id" className="btn btn-sm btn-primary">Visualizar</a>
-                </div>
-                <div className="m-2 float-right">
-                    <a href="/boards/edit/this.id" className="btn btn-sm btn-secondary">Editar</a>
-                </div>
-                <div className="m-2 float-right">
-                    <a href="/boards/destroy/hi" className="btn btn-sm btn-danger">Excluir</a>
-                </div>
-            </div>
-          </a>
-      </div>
-
+      <a href="/boards/new" className="btn btn-success">
+        Novo quadro
+      </a>
+      {boards.map(board => (
+        <BoardItem
+          key={board._id}
+          title={board.name}
+          description={board.description}
+          id={board._id}
+          handleDelete={handleDelete}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default BoardList
+export default BoardList;
