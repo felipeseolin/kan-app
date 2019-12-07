b2691aadec70cd80ca08f1858b9578e752ce211e
felipeseolin <felipe.seolin@hotmail.com>
2019-12-07 19:28:04 -0300

    New routing

--- src/App.js
+++ src/App.js
@@ -1,50 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// Components
-import Login from './pages/Login';
-import BoardList from './pages/BoardList';
-import BoardCreate from './pages/BoardCreate';
-import BoardEdit from './pages/BoardEdit';
-import BoardDetails from './pages/BoardDetails';
-import ListCreate from './pages/ListCreate';
-import ListEdit from './pages/ListEdit';
-import CardCreate from './pages/CardCreate';
-import CardEdit from './pages/CardEdit';
 
-const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Login />
-        </Route>
-        <Route exact path="/boards">
-          <BoardList />
-        </Route>
-        <Route exact path="/boards/new">
-          <BoardCreate />
-        </Route>
-        <Route exact path="/boards/:id/edit">
-          <BoardEdit />
-        </Route>
-        <Route exact path="/boards/:id">
-          <BoardDetails />
-        </Route>
-        <Route exact path="/boards/:idBoard/lists/new">
-          <ListCreate />
-        </Route>
-        <Route exact path="/boards/:idBoard/lists/:idList/edit">
-          <ListEdit />
-        </Route>
-        <Route exact path="/boards/:idBoard/lists/:idList/cards/new">
-          <CardCreate />
-        </Route>
-        <Route exact path="/boards/:idBoard/lists/:idList/cards/:idCard/edit">
-          <CardEdit />
-        </Route>
-      </Switch>
-    </Router>
-  );
-};
+import Routes from './routes';
+
+const App = () => <Routes />;
 
 export default App;
