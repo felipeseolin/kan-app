73c6fcc112c08e71094f63f8a2b43c6df55cba3c
felipeseolin <felipe.seolin@hotmail.com>
2019-12-07 23:13:52 -0300

    Auth route

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import PrivateRoute from './auth';
 // Components
 import Login from './pages/Login';
+import Register from './pages/Register';
 import BoardList from './pages/BoardList';
 import BoardCreate from './pages/BoardCreate';
 import BoardEdit from './pages/BoardEdit';
@@ -16,33 +18,32 @@ import CardEdit from './pages/CardEdit';
 const Routes = () => (
   <Router>
     <Switch>
-      <Route exact path="/">
-        <Login />
-      </Route>
-      <Route exact path="/boards">
-        <BoardList />
-      </Route>
-      <Route exact path="/boards/new">
-        <BoardCreate />
-      </Route>
-      <Route exact path="/boards/:id/edit">
-        <BoardEdit />
-      </Route>
-      <Route exact path="/boards/:id">
-        <BoardDetails />
-      </Route>
-      <Route exact path="/boards/:idBoard/lists/new">
-        <ListCreate />
-      </Route>
-      <Route exact path="/boards/:idBoard/lists/:idList/edit">
-        <ListEdit />
-      </Route>
-      <Route exact path="/boards/:idBoard/lists/:idList/cards/new">
-        <CardCreate />
-      </Route>
-      <Route exact path="/boards/:idBoard/lists/:idList/cards/:idCard/edit">
-        <CardEdit />
-      </Route>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <PrivateRoute exact path="/boards" component={BoardList} />
+      <PrivateRoute exact path="/boards/new" component={BoardCreate} />
+      <PrivateRoute exact path="/boards/:id/edit" component={BoardEdit} />
+      <PrivateRoute exact path="/boards/:id" component={BoardDetails} />
+      <PrivateRoute
+        exact
+        path="/boards/:idBoard/lists/new"
+        component={ListCreate}
+      />
+      <PrivateRoute
+        exact
+        path="/boards/:idBoard/lists/:idList/edit"
+        component={ListEdit}
+      />
+      <PrivateRoute
+        exact
+        path="/boards/:idBoard/lists/:idList/cards/new"
+        component={CardCreate}
+      />
+      <PrivateRoute
+        exact
+        path="/boards/:idBoard/lists/:idList/cards/:idCard/edit"
+        component={CardEdit}
+      />
     </Switch>
   </Router>
 );
