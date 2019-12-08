da609dc9a817b673c04237a6aef001e7b465c9c5
felipeseolin <felipe.seolin@hotmail.com>
2019-12-08 00:47:34 -0300

    Added authorization header

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    authorization: `Bearer ${localStorage.getItem('@kan/token')}`,
+  },
 });
 
 export default api;
